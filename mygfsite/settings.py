dda88edb12e3e02abb6408a8425b8f18933ceb34
Omar Kanteh <125144887+okanteh1-gsu@users.noreply.github.com>
2025-02-17 13:15:37 -0500

    Add Render host to ALLOWED_HOSTS

--- mygfsite/settings.py
+++ mygfsite/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-3swe+!nhv4m$kv3!wcg_^9jh)dgf*%i(h5uh#3$@4s4a(e7ux*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gf-s-website-1.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 
